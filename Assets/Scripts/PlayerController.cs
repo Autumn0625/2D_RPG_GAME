3ef7e864e94c3e7eea9cdcaf9151476fabfe98ad
autumn0625 <chiou255443@gmail.com>
2023-09-21 21:40:40 +0800

    PlayerControl重作
    climb無法實現所以重做control的部分

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,125 +5,201 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private float InputX;
-    
+    public float runSpeed = 5f;
+    public float jumpSpeed;
+    public float doubleJumpSpeed;
+    //public float climbSpeed;
+
+    private BoxCollider2D myfeet;
     private Rigidbody2D rig;
     private Animator ani;
     private CapsuleCollider2D cap;
-
+    private bool isGround;
+    private bool canDoubleJump;
     public float restoreTime;
-    public Transform groundpoint;
-    public Transform attackpoint;
-    public float attackRange = 3f;
-
-    public LayerMask groundMask, enemymask;
+    //public LayerMask groundMask, enemymask;
     
-    private bool isFlip = false;
-    private bool isGrounded= false;
-    private bool canAttack = true;
-    private bool isOneWayPlatform;
+    //private bool isFlip = false;
+    
+    //private bool canAttack = true;
+    //private bool isOneWayPlatform;
+
+    //private bool isLadder;
+    //private bool isClimbing;
+
+    //private float playerGravity;
 
     public void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
-        cap = GetComponent<CapsuleCollider2D>();
+        myfeet = GetComponent<BoxCollider2D>();
+    //    cap = GetComponent<CapsuleCollider2D>();
+    //    playerGravity = rig.gravityScale;
     }
     public void Update()
     {
-        rig.velocity = new Vector2(moveSpeed * InputX, rig.velocity.y);
+        Flip();
+        Run();
+        Jump();
+        //Attack();
+        CheckGrounded();
+        SwitchAnimation();
+        //CheckAirStatus();
+        //OneWayPlatformCheck();
 
-        //判斷 使用者是否有輸入移動控制 有的話 讓腳色進行跑步動畫的播放
-        ani.SetBool("isRun", Mathf.Abs(rig.velocity.x) > 0);
-        ani.SetBool("isGrounded", isGrounded);
-        ani.SetFloat("yVelocity", rig.velocity.y);
 
-        OneWayPlatformCheck();
-
-        if (!isFlip)
+        //isOneWayPlatform = cap.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
+        //isGrounded = Physics2D.OverlapCircle(groundpoint.position, .2f,groundMask) || cap.IsTouchingLayers(LayerMask.GetMask("MovingPlatform")) || cap.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
+        //canAttack = isGrounded;
+        //CheckLadder();
+    }
+    //void CheckLadder()
+    //{
+    //    isLadder = cap.IsTouchingLayers(LayerMask.GetMask("Ladder"));
+    //}
+    public void CheckGrounded()
+    {
+        isGround = myfeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        Debug.Log(isGround);
+    }
+    public void Flip()
+    {
+        bool playerHasXAxis = Mathf.Abs(rig.velocity.x) > Mathf.Epsilon;
+        if (playerHasXAxis)
         {
-            if(rig.velocity.x < 0)
+            if(rig.velocity.x > 0.1f)
             {
-                isFlip = true;
-                transform.Rotate(0.0f, 180f, 0.0f);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            if (rig.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
             }
         }
-        else
+    }
+    public void Run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(moveDir * runSpeed, rig.velocity.y);
+        rig.velocity = playerVel;
+        bool playerHasXaxisSpeed = Mathf.Abs(rig.velocity.x) > Mathf.Epsilon;
+        ani.SetBool("Run", playerHasXaxisSpeed);
+    }
+    public void Jump()
+    {
+        if(Input.GetButtonDown("Jump")) 
         {
-            if (rig.velocity.x > 0)
+            if(isGround)
             {
-                isFlip = false;
-                transform.Rotate(0.0f, 180f, 0.0f);
+                ani.SetBool("Jump", true);
+                Vector2 jumpVel = new Vector2(0.0f, jumpSpeed);
+                rig.velocity = Vector2.up * jumpVel;
+                canDoubleJump = true;
+            }
+            else
+            {
+                if(canDoubleJump)
+                {
+                    ani.SetBool("DoubleJump", true);
+                    Vector2 doubleJumpvel = new Vector2(0.0f, doubleJumpSpeed);
+                    rig.velocity = Vector2.up * doubleJumpvel;
+                    canDoubleJump = false;
+                }
             }
         }
-        isOneWayPlatform = cap.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
-        isGrounded = Physics2D.OverlapCircle(groundpoint.position, .2f,groundMask) || cap.IsTouchingLayers(LayerMask.GetMask("MovingPlatform")) || cap.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
-        canAttack = isGrounded;
+    }
+    //public void Attack()
+    //{
+    //    if (Input.GetButtonDown("Attack"))
+    //    {
+    //        ani.SetTrigger("Attack");
+    //    }
+    //}
+    public void SwitchAnimation()
+    {
+        ani.SetBool("Idle", false);
+        if (ani.GetBool("Jump"))
+        {
+            if(rig.velocity.y < 0.0f)
+            {
+                ani.SetBool("Jump", false);
+                ani.SetBool("Fall", true);
+            }
+        }
+        else if (isGround)
+        {
+            ani.SetBool("Fall", false);
+            ani.SetBool("Idle", true);
+        }
+
+        if (ani.GetBool("DoubleJump"))
+        {
+            if (rig.velocity.y < 0.0f)
+            {
+                ani.SetBool("DoubleJump", false);
+                ani.SetBool("DoubleFall", true);
+            }
+        }
+        else if (isGround)
+        {
+            ani.SetBool("DoubleFall", false);
+            ani.SetBool("Idle", true);
+        }
     }
 
+    //public void Climb()
+    //{
+    //    if (isLadder)
+    //    {
+    //        float moveY = Input.GetAxis("Vertical");
+    //        Debug.Log(moveY);
+    //        if (moveY > 0.5f || moveY < -0.5)
+    //        {
+    //            ani.SetBool("Climbing", true);
+    //            rig.gravityScale = 0.0f;
+    //            rig.velocity = new Vector2(rig.velocity.x, moveY * climbSpeed);
+    //        }
+    //        else
+    //        {
+    //            ani.SetBool("Climbing", false);
+    //            rig.velocity = new Vector2(rig.velocity.x, 0.0f);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        ani.SetBool("Climbing", false);
+    //        rig.gravityScale = playerGravity;
+    //    }
+
+        
+    //}
     
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        InputX = context.ReadValue<Vector2>().x;
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (isGrounded)
-        {
-            rig.velocity = new Vector2(rig.velocity.x, 6);
-        }
-    }
-    public void Attack(InputAction.CallbackContext context)
-    {
-        //檢查玩家是否可以攻擊
-        if (canAttack)
-        {
-            ani.SetBool("attack", true);
-        }
-    }
-    private void CheckAttackHit()
-    {
-        //宣告 打到的目標物件 => 有可能是空的 如果是空的 => 沒打到東西 , 如果有 => 傳送指令給被攻擊者
-        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemymask);
-
-        foreach (Collider2D collider in detectedObjects)
-        {
-            Debug.Log(collider.gameObject.name);
-            collider.gameObject.SendMessage("onDamage", 10.0f);
-        }
-    }
-    public void EndAttack()
-    {
-        ani.SetBool("attack", false);
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(groundpoint.position, .2f);
-        Gizmos.DrawWireSphere(attackpoint.position, attackRange);
-    }
     
-    void OneWayPlatformCheck()
-    {
-        if (isGrounded && gameObject.layer != LayerMask.NameToLayer("player"))
-        {
-            gameObject.layer = LayerMask.NameToLayer("player");
-        }
-        float moveY = Input.GetAxis("Vertical");
-        if (isOneWayPlatform && moveY <- 0.1f)
-        {
-            gameObject.layer = LayerMask.NameToLayer("OneWayPlatform");
-            Invoke("RestorePlayerLayer", restoreTime);
-        }
-    }
+    //void OneWayPlatformCheck()
+    //{
+    //    if (isGrounded && gameObject.layer != LayerMask.NameToLayer("player"))
+    //    {
+    //        gameObject.layer = LayerMask.NameToLayer("player");
+    //    }
+    //    float moveY = Input.GetAxis("Vertical");
+    //    if (isOneWayPlatform && moveY <- 0.1f)
+    //    {
+    //        gameObject.layer = LayerMask.NameToLayer("OneWayPlatform");
+    //        Invoke("RestorePlayerLayer", restoreTime);
+    //    }
+    //}
 
-    void RestorePlayerLayer()
-    {
-        if(!isGrounded && gameObject.layer != LayerMask.NameToLayer("player"))//Player改成player
-        {
-            gameObject.layer = LayerMask.NameToLayer("player");
-        }
-    }
+    //void RestorePlayerLayer()
+    //{
+    //    if(!isGrounded && gameObject.layer != LayerMask.NameToLayer("player"))//Player改成player
+    //    {
+    //        gameObject.layer = LayerMask.NameToLayer("player");
+    //    }
+    //}
+
+    //void CheckAirStatus()
+    //{
+    //    isClimbing = ani.GetBool("Climbing");
+    //}
 }
